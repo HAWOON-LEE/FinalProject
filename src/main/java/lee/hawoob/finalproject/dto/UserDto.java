ac590a67fc9d8bd69c3e8611c9cba2749ae83e98
minsu0922 <95674634+minsu0922@users.noreply.github.com>
2022-05-03 22:03:44 +0900

    마무리3

--- src/main/java/lee/hawoob/finalproject/dto/UserDto.java
+++ src/main/java/lee/hawoob/finalproject/dto/UserDto.java
@@ -4,10 +4,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class UserDto {
 
+    @NotNull(message = "닉네임을 입력해주세요")
     private String nickname;
     private String mbti;
     private String email;
